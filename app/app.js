797a30a840953aeec5b8272896a3cd3a24f3e49a
MaxIurov <maxiurov@gmail.com>
2016-10-10 02:03:12 +0300

    some changes
    shit does not work anyway

--- app/app.js
+++ app/app.js
@@ -1,14 +1,38 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+var myApp = angular.module('myApp', [
+	'ui.router'
+])
+.config(['$urlRouterProvider','$stateProvider',function($urlRouterProvider,$stateProvider) {
+	$urlRouterProvider.otherwise('/tab1');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+	$stateProvider
+	.state('tab1',{
+		name: 'tab1',
+		url: '/tab1',
+		temlateUrl: 'tab1/tab1.html'
+	})
+	.state('tab2',{
+		name: 'tab2',
+		url: '/tab2',
+		temlateUrl: 'tab2/tab2.html'
+	});
 }]);
+/*.config(function($stateProvider) {
+  var helloState = {
+    name: 'hello',
+    url: '/hello',
+    template: '<h3>hello world!</h3>'
+  }
+
+  var aboutState = {
+    name: 'about',
+    url: '/about',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(aboutState);
+});*/
